c5290a0fee42819900f7297f909e3ac5e19dc47a
MisanaProger <misanaproger@gmail.com>
2023-11-17 23:04:34 +0500

    Added All key codes

--- src/window_manager/event_handler/keyboard_event_handler.rs
+++ src/window_manager/event_handler/keyboard_event_handler.rs
@@ -1,6 +1,6 @@
 use std::cell::RefCell;
 
-use xcb::x::Screen;
+use xcb::x::Keycode;
 
 pub struct KeyboardEventHandler {
     input_buffer: RefCell<Vec<KeyCode>>,
@@ -19,90 +19,106 @@ impl KeyboardEventHandler {
             .borrow_mut()
             .push(KeyCode::from(event.detail()))
     }
+
+    pub fn on_relese(&self, event: xcb::x::KeyReleaseEvent) {
+        if self
+            .input_buffer
+            .borrow()
+            .contains(&Keycode::from(event.detail()))
+        {}
+    }
 }
 
 impl From<u8> for KeyCode {
     fn from(value: u8) -> Self {
+        if let Ok(letter) = Letter::try_from(value) {
+            return Self::Letter(letter);
+        }
+        if let Ok(numpad_key) = Numpad::try_from(value) {
+            return Self::Numpad(numpad_key);
+        }
+        if let Ok(modkey) = ModKey::try_from(value) {
+            return Self::Modkey(modkey);
+        }
+        if let Ok(functional_key) = Functional::try_from(value) {
+            return Self::Functional(functional_key);
+        }
+
         match value {
-            8 => Self::Space,
-            9 => Self::Esc,
-            10 => Self::Num1,
-            11 => Self::Num2,
-            12 => Self::Num3,
-            13 => Self::Num4,
-            14 => Self::Num5,
-            15 => Self::Num6,
-            16 => Self::Num7,
-            17 => Self::Num8,
-            18 => Self::Num9,
-            19 => Self::Num0,
-            20 => Self::Minus,
-            21 => Self::PlusAndEqual,
+            9 => Self::Escape,
+            10..=18 => Self::Num(1 + value - 10),
+            19 => Self::Num(0),
             22 => Self::BackSpace,
             23 => Self::Tab,
-            24 => Self::Q,
-            25 => Self::W,
-            26 => Self::E,
-            27 => Self::R,
-            28 => Self::T,
-            29 => Self::Y,
-            30 => Self::U,
-            31 => Self::I,
-            32 => Self::O,
-            33 => Self::P,
-            34 => Self::BracketLeft,
-            35 => Self::BracketRight,
             36 => Self::Return,
-            37 => Self::LeftControl,
-            38 => Self::A,
-            39 => Self::S,
-            40 => Self::D,
-            41 => Self::F,
-            42 => Self::G,
-            43 => Self::H,
-            44 => Self::J,
-            45 => Self::K,
-            46 => Self::L,
-            47 => Self::Colon,
-            48 => Self::Apostrophe,
-            49 => Self::Grave,
-            50 => Self::LeftShift,
-            51 => Self::BackSlash,
-            52 => Self::Z,
-            53 => Self::X,
-            54 => Self::C,
-            55 => Self::V,
-            56 => Self::B,
-            57 => Self::N,
-            58 => Self::M,
-            59 => Self::Less,
-            60 => Self::Greater,
-            61 => Self::Slash,
-            62 => Self::RightShift,
-            63 =>Self::NumpadMultiply,
             65 => Self::Space,
             66 => Self::CapsLock,
-            67 => Self::F1,
-            68=>Self::F2,
-            69 => Self::F3,
-            70 => Self::F4,
-            71 => Self::F5,
-            72=>Self::F6,
-            73=>Self::F7,
-            74=>Self::F8,
-            75=>Self::F9,
-            76=>Self::F10,
-            77=>Self::NumLock,
-            78=>Self::ScrollLock,
-            79 =>Self::NumpadHome,
-            80 =>Self::NumpadUp,
-            81=>Self::NumpadPageUp,
-            ``
+            67..=76 => Self::F(1 + value - 67),
+            77 => Self::NumLock,
+            95..=96 => Self::F(11 + value - 95),
+
+            107 | 218 => Self::Print,
+            110 => Self::Home,
+            111 => Self::Up,
+            112 => Self::PageUp,
+            113 => Self::Left,
+            114 => Self::Right,
+            115 => Self::End,
+            116 => Self::Down,
+            117 => Self::PageUp,
+            118 => Self::Insert,
+            119 => Self::Delete,
+            127 => Self::Pause,
+
+            135 => Self::Menu,
+            136 => Self::Cancel,
+            137 | 190 => Self::Redo,
+            138 => Self::SunProps,
+            139 => Self::Undo,
+            140 => Self::SunFront,
+            144 => Self::Find,
+            146 => Self::Help,
         }
     }
 }
 
 pub enum KeyCode {
+    BackSpace,
+    Cancel,
+    CapsLock,
+    Delete,
+    Down,
+    End,
+    Escape,
+    F(u8),
+    Find,
+    Functional(Functional),
+    Help,
+    Home,
+    Insert,
+    Left,
+    Letter(Letter),
+    Menu,
+    Modkey(ModKey),
+    Num(u8),
+    NumLock,
+    Numpad(Numpad),
+    PageDown,
+    PageUp,
+    Pause,
+    Print,
+    Redo,
+    Return,
+    Right,
+    Space,
+    SunFront,
+    SunProps,
+    Tab,
+    Undo,
+    Up,
+}
+
+pub enum Letter {
     A,
     B,
     C,
@@ -129,151 +145,423 @@ pub enum KeyCode {
     X,
     Y,
     Z,
+}
 
-    Less,
-    Greater,
-    BracketLeft,
-    BracketRight,
-    BackSlash,
-    Slash,
-    Esc,
-    LeftControl,
-    RightControl,
-    LeftAlt,
-    RightAlt,
-    LeftShift,
-    RightShift,
-    Return,
-    BackSpace,
-    Space,
-    Tab,
-    CapsLock,
-    LeftSuper,
-    RightSuper,
-    Menu,
-    NumLock,
-    Grave,
-    Colon,
-    Apostrophe,
-    ScrollLock,
+impl TryFrom<u8> for Letter {
+    fn try_from(value: u8) -> Result<Letter, Self::Error> {
+        Ok(match value {
+            24 => Self::Q,
+            25 => Self::W,
+            26 => Self::E,
+            27 => Self::R,
+            28 => Self::T,
+            29 => Self::Y,
+            30 => Self::U,
+            31 => Self::I,
+            32 => Self::O,
+            33 => Self::P,
+            52 => Self::Z,
+            53 => Self::X,
+            54 => Self::C,
+            55 => Self::V,
+            56 => Self::B,
+            57 => Self::N,
+            58 => Self::M,
+            38 => Self::A,
+            39 => Self::S,
+            40 => Self::D,
+            41 => Self::F,
+            42 => Self::G,
+            43 => Self::H,
+            44 => Self::J,
+            45 => Self::K,
+            46 => Self::L,
+            _ => return Err(()),
+        })
+    }
 
-    End,
-    Home,
-    PageUp,
-    PageDown,
-    Pause,
-    Delete,
-    Insert,
-    Print,
+    type Error = ();
+}
 
-    Up,
-    Down,
+pub enum ModKey {
+    Shift(KeyPosition),
+    Control(KeyPosition),
+    Alt(KeyPosition),
+    Super(KeyPosition),
+    Meta(KeyPosition),
+    Hyper,
+}
+
+impl TryFrom<u8> for ModKey {
+    type Error = ();
+
+    fn try_from(value: u8) -> Result<Self, Self::Error> {
+        Ok(match value {
+            50 => Self::Shift(KeyPosition::Left),
+            62 => Self::Shift(KeyPosition::Right),
+            37 => Self::Control(KeyPosition::Left),
+            105 => Self::Control(KeyPosition::Right),
+            207 => Self::Hyper,
+            64 | 205 => Self::Meta(KeyPosition::Left),
+            108 => Self::Meta(KeyPosition::Right),
+            133 | 206 => Self::Super(KeyPosition::Left),
+            134 => Self::Super(KeyPosition::Right),
+            64 | 204 => Self::Alt(KeyPosition::Left),
+            108 => Self::Alt(KeyPosition::Right),
+            _ => return Err(()),
+        })
+    }
+}
+
+pub enum KeyPosition {
     Left,
     Right,
+}
 
-    F1,
-    F2,
-    F3,
-    F4,
-    F5,
-    F6,
-    F7,
-    F8,
-    F9,
-    F10,
-    F11,
-    F12,
+pub enum Symbols {
+    Apostrophe,
+    BackSlash,
 
-    Num1,
-    Num2,
-    Num3,
-    Num4,
-    Num5,
-    Num6,
-    Num7,
-    Num8,
-    Num9,
-    Num0,
+    BracketLeft,
+    BracketRight,
+    Colon,
+    Grave,
+    Greater,
+    Less,
+    LessGreaterBar,
+    Linefeed,
     Minus,
+    ParenLeft,
+    ParenRight,
     PlusAndEqual,
+    PlusMinus,
+    Slash,
+}
 
-    NumpadNum1,
-    NumpadNum2,
-    NumpadNum3,
-    NumpadNum4,
-    NumpadNum5,
-    NumpadNum6,
-    NumpadNum7,
-    NumpadNum8,
-    NumpadNum9,
-    NumpadNum0,
-    NumpadEnter,
-    NumpadPlus,
-    NumpadMinus,
-    NumpadDevide,
-    NumpadMultiply,
-    NumpadDelete,
-    NumpadDecimal,
-    NumpadUp,
-    NumpadDown,
-    NumpadLeft,
-    NumpadRight,
-    NumpadEnd,
-    NumpadHome,
-    NumpadPageUp,
-    NumpadPageDown,
-    NumpadInsert,
+pub enum Numpad {
+    Num(u8),
+    Devide,
+    Substract,
+    Add,
+    Multiply,
+    NumLock,
+    Enter,
+    Equal,
+    Decimal,
+    Delete,
+}
 
-    FunctionalPowerOff,
-    FunctionalAudioLowerVolume,
-    FunctionalRiseAudioVolume,
-    FunctionalMute,
-    FunctionalAudioPause,
-    FunctionalAudioStop,
-    FunctionalAudioMedia,
-    FunctionalAudioPlay,
-    FunctionalAudioRecord,
-    FunctionalAudioNext,
-    FunctionalAudioPrev,
-    FunctionalAudioFarward,
-    FunctionalMicrophoneMutea,
+impl TryFrom<u8> for Symbols {
+    fn try_from(value: u8) -> Result<Self, Self::Error> {
+        Ok(match value {
+            47 => Self::Colon,
+            48 => Self::Apostrophe,
+            49 => Self::Grave,
+            51 => Self::BackSlash,
+            59 => Self::Less,
+            60 => Self::Greater,
+            61 => Self::Slash,
+            109 => Self::Linefeed,
+            34 => Self::BracketLeft,
+            35 => Self::BracketRight,
+            187 => Self::ParenLeft,
+            188 => Self::ParenRight,
+            20 => Self::Minus,
+            94 => Self::LessGreaterBar,
+            21 => Self::PlusAndEqual,
+            126 => Self::PlusMinus,
+            _ => return Err(()),
+        })
+    }
+    type Error = ();
+}
 
-    FunctionlDisplay,
-    FunctionalDisplayOff,
+impl TryFrom<u8> for Numpad {
+    fn try_from(value: u8) -> Result<Self, Self::Error> {
+        Ok(match value {
+            79..=81 => Numpad::Num(7 + value - 79),
+            83..=85 => Numpad::Num(4 + value - 83),
+            87..=89 => Numpad::Num(1 + value - 87),
+            90 => Numpad::Num(0),
+            63 => Numpad::Multiply,
+            82 => Numpad::Substract,
+            86 => Numpad::Add,
+            91 => Numpad::Delete,
+            104 => Numpad::Enter,
+            106 => Numpad::Devide,
+            125 => Numpad::Equal,
+            129 => Numpad::Decimal,
+            _ => return Err(()),
+        })
+    }
+    type Error = ();
+}
 
-    FunctionalMonitorBrightnessDown,
-    FunctionalMonitorBrightnessUp,
-    FunctionalMonitorBrightnessCycle,
+pub enum Functional {
+    Audio(Audio),
+    Back,
+    Battery,
+    Bluetooth,
+    BrightnessAuto,
+    Calculator,
+    Close,
+    Copy,
+    Cut,
+    DOS,
+    Display,
+    DisplayOff,
+    Documents,
+    Eject,
+    Explorer,
+    Farward,
+    Favourite,
+    Finance,
+    Game,
+    Go,
+    HomePage,
+    KBD(KBD),
+    Kill,
+    Launch(u8),
+    LaunchA,
+    LaunchB,
+    Mail,
+    MailFarward,
+    Menu,
+    Messanger,
+    Monitor(Monitor),
+    MyComputer,
+    New,
+    NextVMode,
+    Open,
+    Paste,
+    Phone,
+    PowerOff,
+    PrevVMode,
+    Reload,
+    Reply,
+    RotateWindows,
+    Save,
+    ScreenSaver,
+    ScrollDown,
+    ScrollLock,
+    ScrollUp,
+    Search,
+    Send,
+    Shop,
+    Sleep,
+    Suspend,
+    TaskPlane,
+    Tools,
+    TouchPad(TouchPad),
+    UWB,
+    UnRecognized,
+    WLAN,
+    WWAN,
+    WWW,
+    WakeUp,
+    WebCamera,
+    Xref,
+}
 
-    FunctionalOpen,
-    FunctionalClose,
-    FunctionalCopy,
-    FunctionalCut,
-    FunctionalPaste,
+impl TryFrom<u8> for Functional {
+    type Error = ();
 
-    FunctionalSleep,
-    FunctionalWakeUp,
-    FunctionalCalculator,
-    FunctionalSend,
-    FunctionalScreenSaver,
-    FunctionalMail,
-    FunctionalFavorites,
-    FunctionalTaskPlane,
-    FunctionalLaunch1,
-    FunctionalLaunch2,
-    FunctionalMyComputer,
-    FunctionalEject,
-    FunctionalBack,
-    FunctionalFarward,
-    FunctionalRotateWindows,
-    FunctionalExplorer,
-    FunctionalHomePage,
-    FunctionalReload,
-    FunctionalTools,
-    FunctionalPhone,
-    FunctionalScrollUp,
-    FunctionalScrollDown,
-    FunctionalNew,
-    FunctionalSearch,
-    FunctionalReply,
+    fn try_from(value: u8) -> Result<Self, Self::Error> {
+        if let Ok(audio_key) = Audio::try_from(value) {
+            return Ok(Self::Audio(audio_key));
+        }
+
+        if let Ok(kbd_key) = KBD::try_from(value) {
+            return Ok(Self::KBD(kbd_key));
+        }
+
+        if let Ok(monitor_key) = Monitor::try_from(value) {
+            return Ok(Self::Monitor(monitor_key));
+        }
+
+        if let Ok(touchpad_key) = TouchPad::try_from(value) {
+            return Ok(Self::TouchPad(touchpad_key));
+        }
+
+        Ok(match value {
+            255 => Self::Kill,
+            254 => Self::WWAN,
+            253 => Self::DisplayOff,
+            250 => Self::PrevVMode,
+            249 => Self::NextVMode,
+            247 => Self::UWB,
+            246 => Self::WLAN,
+            245 => Self::Bluetooth,
+            244 => Self::Battery,
+            243 => Self::Documents,
+            242 => Self::Save,
+            241 => Self::MailFarward,
+            240 => Self::Reply,
+            153 | 239 => Self::Send,
+            235 => Self::Display,
+            229 => Self::Shop,
+            228 => Self::Game,
+            227 => Self::Finance,
+            226 => Self::Go,
+            225 => Self::Search,
+            224 => Self::Messanger,
+            163 | 223 => Self::Mail,
+            220 => Self::WebCamera,
+            214 => Self::Close,
+            213 => Self::Suspend,
+            212 => Self::LaunchB,
+            210..=211 => Self::Launch(3 + value - 210),
+            192..=196 => Self::Launch(5 + value - 192),
+            179 | 191 => Self::Tools,
+            189 => Self::New,
+            186 => Self::ScrollDown,
+            185 => Self::ScrollUp,
+            182 => Self::Close,
+            181 => Self::Reload,
+            180 => Self::HomePage,
+            177 => Self::Phone,
+            169 | 170 => Self::Eject,
+            167 => Self::Farward,
+            166 => Self::Back,
+            165 => Self::MyComputer,
+            164 => Self::Favourite,
+            162 => Self::TaskPlane,
+            161 => Self::RotateWindows,
+            160 => Self::ScreenSaver,
+            159 => Self::DOS,
+            158 => Self::WWW,
+            156..=157 => Self::Launch(1 + value - 156),
+            155 => Self::Xref,
+            153 => Self::Send,
+            152 => Self::Explorer,
+            151 => Self::WakeUp,
+            150 => Self::Sleep,
+            148 => Self::Calculator,
+            147 => Self::Menu,
+            145 => Self::Cut,
+            143 => Self::Paste,
+            142 => Self::Open,
+            141 => Self::Copy,
+            128 => Self::LaunchA,
+            124 => Self::PowerOff,
+            78 => Self::ScrollLock,
+
+            _ => return Err(()),
+        })
+    }
+}
+
+pub enum KBD {
+    ToggleLight,
+    BrightnessUp,
+    BrightnessDown,
+}
+
+impl TryFrom<u8> for KBD {
+    type Error = ();
+
+    fn try_from(value: u8) -> Result<Self, Self::Error> {
+        Ok(match value {
+            236 => Self::ToggleLight,
+            237 => Self::BrightnessDown,
+            238 => Self::BrightnessUp,
+            _ => return Err(()),
+        })
+    }
+}
+
+pub enum Monitor {
+    RiseBrightness,
+    LowerBrightness,
+    BrightnessCycle,
+}
+
+impl TryFrom<u8> for Monitor {
+    type Error = ();
+
+    fn try_from(value: u8) -> Result<Self, Self::Error> {
+        Ok(match value {
+            232 => Self::LowerBrightness,
+            233 => Self::RiseBrightness,
+            251 => Self::BrightnessCycle,
+            _ => return Err(()),
+        })
+    }
+}
+
+pub enum TouchPad {
+    On,
+    Off,
+    Toggle,
+}
+
+impl TryFrom<u8> for TouchPad {
+    type Error = ();
+
+    fn try_from(value: u8) -> Result<Self, Self::Error> {
+        Ok(match value {
+            199 => Self::Toggle,
+            200 => Self::On,
+            201 => Self::Off,
+            _ => return Err(()),
+        })
+    }
+}
+
+pub enum Audio {
+    Volume(AudioVolume),
+    Play,
+    Next,
+    Prev,
+    Stop,
+    Pause,
+    MuteMicrophone,
+    Record,
+    Preset,
+    Rewind,
+    Forward,
+    Media,
+}
+
+impl TryFrom<u8> for Audio {
+    type Error = ();
+
+    fn try_from(value: u8) -> Result<Self, Self::Error> {
+        if let Ok(volume) = AudioVolume::try_from(value) {
+            return Ok(Self::Volume(volume));
+        }
+        Ok(match value {
+            172 | 208 | 215 => Self::Play,
+            171 => Self::Next,
+            209 => Self::Pause,
+            174 => Self::Stop,
+            198 => Self::MuteMicrophone,
+            175 => Self::Record,
+            173 => Self::Prev,
+            221 => Self::Preset,
+            176 => Self::Rewind,
+            216 => Self::Forward,
+            234 => Self::Media,
+            _ => return Err(()),
+        })
+    }
+}
+
+pub enum AudioVolume {
+    Lower,
+    Rise,
+    Mute,
+}
+
+impl TryFrom<u8> for AudioVolume {
+    type Error = ();
+
+    fn try_from(value: u8) -> Result<Self, Self::Error> {
+        Ok(match value {
+            121 => Self::Mute,
+            122 => Self::Lower,
+            123 => Self::Rise,
+            _ => return Err(()),
+        })
+    }
 }
