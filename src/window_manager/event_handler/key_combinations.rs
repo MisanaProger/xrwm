b192370e0f93cc802aeafc50aa84eba575dae6cc
MisanaProger <misanaproger@gmail.com>
2023-11-18 19:50:53 +0500

    Added key combinations

---
+++ src/window_manager/event_handler/key_combinations.rs
@@ -0,0 +1,22 @@
+use std::str::FromStr;
+
+use super::keyboard_event_handler::KeyCode;
+
+pub struct KeyCombination {
+    combination: Vec<KeyCode>,
+    command: String,
+}
+
+fn parce_combination(combination: String) -> Result<Vec<KeyCode>, ()> {
+    let keys_result: Vec<Result<KeyCode, ()>> =
+        combination.split("+").map(|key_string| todo!()).collect();
+    let mut keys = Vec::new();
+    for key in keys_result {
+        if let Ok(key) = key {
+            keys.push(key);
+            continue;
+        }
+        return Err(());
+    }
+    Ok(keys)
+}
